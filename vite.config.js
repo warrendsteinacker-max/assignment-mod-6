4268ca7dc06e568c1446602de14fcc3762f105f7
Warren <warrendsteinacker@gmail.com>
2026-02-13 20:10:29 -0500

    nnn

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react' // or your specific framework
 
 export default defineConfig({
   plugins: [react()],
-  base: '/6-assig/', // REPLACE 'my-repo-name' with your actual GitHub repo name
+  base: '/assignment-mod-6/', // REPLACE 'my-repo-name' with your actual GitHub repo name
 })
 //////////////////////////////////
